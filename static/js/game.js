2c93fc1ea9376ccb51d91108a4e38e91ede28cb3
Eugene Rybalkini <kashelot@gmail.com>
2012-06-30 22:45:50 +0300

    game.js

--- static/js/game.js
+++ static/js/game.js
@@ -34,7 +34,7 @@ Game.prototype.dispatchSocketEvent = function(event) {
         } else {
             if (event[i].player) {
                 var player =this.players[event[i].player]
-                if ($.inArray(cmd, ['move', 'end']) == -1) {
+                if ($.inArray(cmd, ['move', 'rotate']) != -1) {
                     if(!player.isAnimated){
                         player[cmd].apply(player,args)
                     }else{
