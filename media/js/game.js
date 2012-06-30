89428f7c67120e74e7628904a2aa85a51274d948
Eugene Rybalkini <kashelot@gmail.com>
2012-06-30 15:25:22 +0300

    - basic game

--- media/js/game.js
+++ media/js/game.js
@@ -1,4 +1,17 @@
-Crafty.c('Player', {
-    init: function() {
-    }
+function Game() {
+    this._players = {
+    };
+}
+
+Game.prototype.init = function(canvas, options) {
+    Crafty.init(canvas);
+            //.canvas.init();
+    console.log('inited...');
+}
+
+$(function() {
+    var gameInstance = new Game();
+        gameInstance.init($('#canvas').get(0), {
+            size : [640, 640]
+        });
 });
