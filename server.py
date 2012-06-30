6763ae355f85807968cf453cc26258801867c1c5
Vitaly Haritonsky <vharitonsky@gmail.com>
2012-06-30 15:40:38 +0300

    fixed ws url compilation

--- server.py
+++ server.py
@@ -4,19 +4,19 @@ import tornado.ioloop
 import tornado.web
 from tornado import websocket
 from tornado import autoreload
-from tornado.template import Template,Loader
-from tornado.options import define,parse_command_line,options
+from tornado.template import Template, Loader
+from tornado.options import define, parse_command_line, options
 
-define(name = 'port', type='int', default='8888', help='run server on designated port')
-define(name = 'host', type='str', default='127.0.0.1', help = 'host the application is running on')
+define(name = 'port', type = 'int', default = '8888', help = 'run server on designated port')
+define(name = 'host', type = 'str', default = '127.0.0.1', help = 'host the application is running on')
 
 
 sockets = []
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        ws_url = 'ws://%(host)s:%(port)s' % options
-        self.render('index.html',ws_url = ws_url)
+        ws_url = 'ws://%(host)s:%(port)s/websocket' % {'host':options.host, 'port':options.port}
+        self.render('index.html', ws_url = ws_url)
 
 class EchoWebSocket(websocket.WebSocketHandler):
     def open(self):
@@ -33,16 +33,16 @@ class EchoWebSocket(websocket.WebSocketHandler):
         print "WebSocket closed %s" % self
 
 application = tornado.web.Application([
-    (r"/",MainHandler),
-    (r"/websocket",EchoWebSocket),
-    (r"/static/(.*)",tornado.web.StaticFileHandler,{'path':'./static'}),
-],static_path = os.path.join(os.path.dirname(__file__),'static')
- ,template_path = os.path.join(os.path.dirname(__file__),'templates')
- ,debug = True)
+    (r"/", MainHandler),
+    (r"/websocket", EchoWebSocket),
+    (r"/static/(.*)", tornado.web.StaticFileHandler, {'path':'./static'}),
+], static_path = os.path.join(os.path.dirname(__file__), 'static')
+ , template_path = os.path.join(os.path.dirname(__file__), 'templates')
+ , debug = True)
 
 if __name__ == "__main__":
     parse_command_line()
     application.listen(options.port)
     autoreload.start()
     tornado.ioloop.IOLoop.instance().start()
-    
+
