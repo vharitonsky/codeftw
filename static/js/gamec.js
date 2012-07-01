b64ef64ca1675ec4a8807ea03484c731cbc41fea
Eugene Rybalkini <kashelot@gmail.com>
2012-07-01 08:48:01 +0300

    game.js

--- static/js/gamec.js
+++ static/js/gamec.js
@@ -74,7 +74,8 @@ Crafty.c('PlayerControls', {
             if (!this.within(0, 0, Crafty.viewport.width, Crafty.viewport.height)) {
                 this.destroy();
             }
-        })
+        });
+        /*
         .collision()
         .onHit("stone", function(e) {
             e[0].obj.destroy();
@@ -84,6 +85,7 @@ Crafty.c('PlayerControls', {
             e[0].obj.destroy();
             this.destroy();
         });
+        */
     }
 });
 
